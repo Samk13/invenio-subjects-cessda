a2d28598b559f7412d5f472cddc4f071ad2391cc
Sam Arbid <samaa2@kth.se>
2024-02-01 00:17:36 +0100

    test get_latest_versions functionality

--- tests/test_fetch_voc.py
+++ tests/test_fetch_voc.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2022 KTH Royal Institute of Technology Sweden
+# Copyright (C) 2022-2024 KTH Royal Institute of Technology Sweden.
 #
 # invenio-subjects-CESSDA is free software, you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file details.
 
-from invenio_subjects_cessda.config import urls
 from invenio_subjects_cessda.fetch_voc import fetch_voc
+from tests.data import urls_to_fetch
 
 
 # @pytest.mark.asyncio
 def test_fetch_voc():
     """Test fetch_voc."""
-    fetch_res = fetch_voc(urls)
-    assert len(fetch_res) == len(urls)
+    fetch_res = fetch_voc(urls_to_fetch)
+    assert len(fetch_res) == len(urls_to_fetch)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2022 KTH Royal Institute of Technology Sweden
+# Copyright (C) 2022-2024 KTH Royal Institute of Technology Sweden.
 #
 # invenio-subjects-CESSDA is free software, you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file details.
 
-from invenio_subjects_cessda.utils import fix_url
-from tests.data import res_data, test_data
+from invenio_subjects_cessda.config import languages
+from invenio_subjects_cessda.utils import fix_url, get_latest_versions
+from tests.data import (
+    res_data,
+    test_data,
+    vocabularies_published_expected_output,
+    vocabularies_published_versions_input,
+)
 
 
 def test_fix_url():
-    """test fix_url"""
+    """Test fix_url."""
     for i, u in enumerate(test_data):
         assert fix_url(u) == res_data[i]
+
+
+def test_get_latest_versions():
+    """Test get latest versions for api URLs"""
+    result = get_latest_versions(vocabularies_published_versions_input, languages)
+    assert result == vocabularies_published_expected_output
